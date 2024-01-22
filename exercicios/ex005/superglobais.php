cdb4e77806a3fb8e6b23676f480c0437e290eabe
Vagner Santana <141592406+vagnerns@users.noreply.github.com>
2024-01-22 18:24:22 -0300

    exercicio005

---
+++ exercicios/ex005/superglobais.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <pre>
+            <?php 
+                echo "<h1>Superglobal GET</h1>";
+                var_dump($_GET);
+
+                echo "<h1>Superglobal POST</h1>";
+                var_dump($_POST);
+
+                echo "<h1>Superglobal REQUEST</h1>";
+                var_dump($_REQUEST);
+            ?>
+        </pre>
+        <button type="button" onclick="javascript:history.go(-1)">Voltar</button>
+    </main>
+</body>
+</html>
