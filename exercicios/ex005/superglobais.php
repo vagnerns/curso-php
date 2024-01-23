a19032e460159f7472e3be46372798e937020c6a
Vagner Santana <141592406+vagnerns@users.noreply.github.com>
2024-01-23 15:18:01 -0300

    Update superglobais.php

--- exercicios/ex005/superglobais.php
+++ exercicios/ex005/superglobais.php
@@ -10,6 +10,11 @@
     <main>
         <pre>
             <?php 
+                setcookie("dia-da-semana", "SEGUNDA", time() + 3600);
+
+                session_start();
+                $_SESSION["teste"] = "FUNCIONOU!";
+
                 echo "<h1>Superglobal GET</h1>";
                 var_dump($_GET);
 
@@ -18,6 +23,24 @@
 
                 echo "<h1>Superglobal REQUEST</h1>";
                 var_dump($_REQUEST);
+
+                echo "<h1>Superglogal COOKIE</h1>";
+                var_dump($_COOKIE);
+
+                echo "<h1>Superglogal SESSION</h1>";
+                var_dump($_SESSION);
+
+                echo "<h1>Superglogal ENV</h1>";
+                var_dump($_ENV);
+                //foreach (getenv() as $c => $v) {
+                //    echo "<br> $c -> $v";
+                //}
+                
+                echo "<h1>Superglogal SERVER</h1>";
+                var_dump($_SERVER);
+
+                echo "<h1>Superglogal GLOBALS</h1>";
+                var_dump($GLOBALS);
             ?>
         </pre>
         <button type="button" onclick="javascript:history.go(-1)">Voltar</button>
